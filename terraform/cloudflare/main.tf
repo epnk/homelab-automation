0193bd04d98453c6fd62c1f08657d87dd622399b
Eric Alekson <eric.alekson@gmail.com>
2023-02-05 16:03:14 -0800

    Initial commit :rocket:

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -84,7 +84,7 @@ resource "cloudflare_record" "ipv4" {
 }
 
 resource "cloudflare_record" "root" {
-  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
+  name    = "local.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
   value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = true
